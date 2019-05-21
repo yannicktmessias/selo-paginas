906fceb18a7ca44605d93a82beb6a0bd3e23ebda
Leonardo Padilha <lcfpadilha@gmail.com>
2019-05-21 18:59:11 -0300

    fix new applicant page

--- scripts/robo.js
+++ scripts/robo.js
@@ -1,12 +1,30 @@
 jQuery(document).ready(function() {
-    var max_links = 30
+    var max_links = 20
     var links = 1
 
     $("#selectAll").click(function() {
         $('input:checkbox').not(this).prop('checked', this.checked);
     });
+
     $("#nextPageSeloForm").click(function() {
         window.location.href = "new_selo_2.html";
+    });
+
+    $("#shouldHaveRepresentative").click(function() {
+        var representativeFormIds = [
+            'representativeName',
+            'representativeCPF',
+            'representativeEmail',
+            'representativePhone',
+            'representativeCell',
+            'representativeStatus0',
+            'representativeStatus1'
+        ]
+
+        for (var i = 0; i < representativeFormIds.length; i++) {
+            $('#' + representativeFormIds[i]).prop('disabled', !this.checked);
+            $('#' + representativeFormIds[i]).prop('required', this.checked);
+        }
     });
 
     $("#addMoreLinkButton").click(function(e) {
